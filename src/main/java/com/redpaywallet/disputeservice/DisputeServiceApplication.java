5e84d16bddfc70ac9a44ebbb22353af6605265df
teeredtech <tolulope.oredein@redtechlimied.com>
2024-04-26 10:51:59 +0100

    first commit

---
+++ src/main/java/com/redpaywallet/disputeservice/DisputeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.redpaywallet.disputeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisputeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisputeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redpaywallet/disputeservice/DisputeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redpaywallet.disputeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisputeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
